ddea0eb8301f300047eb6e7a54285bc832f213f1
Redsnak57 <corentin.malbrun1@gmail.com>
2022-12-20 12:43:55 +0100

    admin panel, bug filtre produit

---
+++ src/Class/Search.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Class;
+
+use App\Entity\Category;
+
+class Search {
+
+    public string $string = "";
+    public $categories = [];
+
+}
---
+++ src/Controller/ProductController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Controller;
+
+use App\Class\Search;
+use App\Entity\Product;
+use App\Form\SearchType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+
+class ProductController extends AbstractController
+{
+    private EntityManagerInterface $manager;
+
+    public function __construct(EntityManagerInterface $manager){
+        $this->manager = $manager;
+    }
+
+    #[Route('/nos-produits', name: 'app_products')]
+    public function index(Request $request): Response
+    {
+
+        $products = $this->manager->getRepository(Product::class)->findAll();
+
+        $search = new Search();
+        $form = $this->createForm(SearchType::class, $search);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            dd($search);
+        }
+
+        return $this->render('product/index.html.twig', [
+            "products" => $products,
+            "form" => $form->createView(),
+        ]);
+    }
+
+    #[Route('/produit/{slug}', name: 'app_product')]
+    public function show($slug)
+    {
+
+        $product = $this->manager->getRepository(Product::class)->findOneBySlug($slug);
+
+        if(!$product){
+            return $this->redirectToRoute("app_products");
+        }
+
+        return $this->render('product/show.html.twig', [
+            "product" => $product,
+        ]);
+    }
+}
